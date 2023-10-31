b33ae8dcdfd0f136aeaa045ed38659c595e9a8ae
shiridharKhatri <khatrishiridhar@gmail.com>
2023-10-31 09:39:34 +0545

    .

--- routes/review.js
+++ routes/review.js
@@ -3,31 +3,19 @@ const Review = require("../models/Review");
 const fetchUser = require("../middleware/fetchUser");
 const User = require("../models/User");
 const router = express.Router();
-const multer = require("multer");
-const moment = require("moment");
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./review-image");
-  },
-  filename: function (req, file, cb) {
-    cb(null, moment().format("MMM Do YY") + "_" + file.originalname);
-  },
-});
-const upload = multer({ storage: storage });
 router.post(
   "/postReview",
-  upload.single("review-img"),
   fetchUser,
   async (req, res) => {
     try {
-      const { review, star, display_name } = req.body;
+      const { review, star, display_name, img } = req.body;
       let user = await User.findById(req.user.id);
       await Review.create({
         review,
         star,
         display_name,
         reviewBy: user.name,
-        img: req.file.filename,
+        img,
       });
       return res
         .status(200)
